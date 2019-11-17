c18877335ae3f6e879e975f8dda69cd8e5bceb3f
marybethl <marylesbirel@gmail.com>
2019-11-16 22:43:20 -0800

    Removed duplicate users + passing through index on songplay data (just made a serial format)

--- etl.py
+++ etl.py
@@ -74,7 +74,7 @@ def process_log_file(cur, filepath):
         songid, artistid = results if results else None, None
 
         # insert songplay record
-        songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent);
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent);
         cur.execute(songplay_table_insert, songplay_data)
 
 def process_data(cur, conn, filepath, func):
