f81a9071cf9f8d75d554ed4e9eb584ba439f1f5f
marybethl <marylesbirel@gmail.com>
2019-11-16 21:52:30 -0800

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,110 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    """
+    Function that will perform ETL song files. Outputs record to 
+    both the songs able and artists table.
+
+    Parameters: 
+    - cur: connection to DB
+    - filepath (string): path of a song data file, ex. song_data/TRAAAAW128F429D538.json
+    """
+
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist();
+    cur.execute(song_table_insert, song_data)
+    
+    # insert artist record
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist();
+    cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    # open log file
+    df = pd.read_json(filepath, lines=True);
+    
+    # filter by NextSong action
+    df = df[df['page'] == 'NextSong'] 
+
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df['ts'], unit='ms') 
+    
+    # insert time data records
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+
+    # When doing sample below on my local, the columns are re-ordered alphabetically
+    time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
+    # to avoid this, going to be explicit
+
+    for i, row in time_df.iterrows():
+        #print(row.values)
+        #print (row.index.values)
+        #print(row)
+        #print(row['start_time'])
+        #cur.execute(time_table_insert, list(row))
+        cur.execute(time_table_insert, [row['start_time'], row['hour'], row['day'], \
+                                        row['week'], row['month'], row['year'], \
+                                        row['weekday']])
+
+    # load user table
+    user_df = pd.DataFrame({'user_id': df['userId'],'first_name': df['firstName'], \
+        'last_name': df['lastName'], 'gender': df['gender'], \
+        'level': df['level']});
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        #cur.execute(user_table_insert, row)
+        cur.execute(user_table_insert, [row['user_id'], row['first_name'], row['last_name'], \
+                                        row['gender'], row['level']])        
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        results = cur.execute(song_select, (row.song, row.artist, row.length))
+        songid, artistid = results if results else None, None
+
+        # insert songplay record
+        songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent);
+        cur.execute(songplay_table_insert, songplay_data)
+
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    #process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
